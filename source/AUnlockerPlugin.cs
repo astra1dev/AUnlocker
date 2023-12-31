b5e7209ac6f0a1dcd835353bab0ddf5ee76b5d5f
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2023-12-31 19:52:39 +0100

    Update v1.1.2
    Add features:
    - No 3s cooldown when sending messages
    - Character limit set to 999 instead of 100
    - Allowing copy-pasting in chat box
    - Allow sending of URLs and Emails 
    - Allow sending of ANY special character

--- source/AUnlockerPlugin.cs
+++ source/AUnlockerPlugin.cs
@@ -1,7 +1,17 @@
+using AmongUs.Data;
+using Assets.CoreScripts;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using Hazel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEngine;
+
 
 namespace AUnlocker;
 
@@ -10,8 +20,6 @@ namespace AUnlocker;
 public partial class AUnlockerPlugin : BasePlugin
 {
     public Harmony Harmony { get; } = new(Id);
-
-    public ConfigEntry<string> ConfigName { get; private set; }
 
     public override void Load()
     { 
@@ -72,6 +80,7 @@ public partial class AUnlockerPlugin : BasePlugin
         }
     }
 
+
     // Friends list patch
     [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFriendsListAllowed))]
     public static class UnlockFriendlist
@@ -83,6 +92,7 @@ public partial class AUnlockerPlugin : BasePlugin
         }
     }
 
+
     // Minor status patch
     [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsMinorOrWaiting))]
     public static class RemoveMinorStatus
@@ -93,6 +103,7 @@ public partial class AUnlockerPlugin : BasePlugin
             return false;
         }
     }
+
 
     // Online patch
     [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsAllowedOnline))]
@@ -112,6 +123,7 @@ public partial class AUnlockerPlugin : BasePlugin
         }
     }
 
+
     // Custom name patch
     [HarmonyPatch(typeof(FullAccount), nameof(FullAccount.CanSetCustomName))]
     public static class CustomNameEnabled
@@ -127,6 +139,110 @@ public partial class AUnlockerPlugin : BasePlugin
 
             // otherwise if custom name is already enabled, do nothing
             return true;
+        }
+    }
+
+
+    // Bypass chat limits (special characters, ctrl+v enabled, etc.)
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.Update))]
+    public static class BypassChatLimit
+    {
+        public static void Postfix(ChatController __instance)
+        {
+            if (!__instance.freeChatField.textArea.hasFocus) return;
+            // no 3s cooldown when sending messages
+            __instance.timeSinceLastMessage = 3f;
+            // if player is host, set limit to 999, otherwise set it to 300 because you might get kicked by server
+            __instance.freeChatField.textArea.characterLimit = AmongUsClient.Instance.AmHost ? 999 : 300;
+            // allow Ctrl + C and Ctrl + V
+            __instance.freeChatField.textArea.AllowPaste = true;
+            
+        }
+    }
+
+
+    // Edit Color indicators for chatbox (only visual)
+    [HarmonyPatch(typeof(FreeChatInputField), nameof(FreeChatInputField.UpdateCharCount))]
+    internal class EditColorIndicators
+    {
+        public static void Postfix(FreeChatInputField __instance)
+        {
+            int length = __instance.textArea.text.Length;
+            // Show new character limit of 300 / 999 instead of vanilla 100 below text field
+            __instance.charCountText.SetText($"{length}/{__instance.textArea.characterLimit}");
+            if (length < (AmongUsClient.Instance.AmHost ? 888 : 250))
+                // Black if not close to limit
+                __instance.charCountText.color = Color.black;
+            else if (length < (AmongUsClient.Instance.AmHost ? 999 : 300))
+                // Yellow if close to limit
+                __instance.charCountText.color = new Color(1f, 1f, 0f, 1f);
+            else
+                // Red if limit reached
+                __instance.charCountText.color = Color.red;
+        }
+    }
+
+
+    // Bypass Anticheat kicking you from the lobby if you send long messages
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSendChat))]
+    public static class BypassAnticheatAndRatelimit
+    {
+        public static bool Prefix(PlayerControl __instance, string chatText, ref bool __result)
+        {
+            // If the player is a client and the HudManager exists, add the chat to the local chat log
+            if (AmongUsClient.Instance.AmClient && DestroyableSingleton<HudManager>.Instance)
+                DestroyableSingleton<HudManager>.Instance.Chat.AddChat(__instance, chatText);
+
+            // Start an RPC (Remote Procedure Call) to send the chat message to other players
+            MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(__instance.NetId, (byte)RpcCalls.SendChat, SendOption.None);
+            messageWriter.Write(chatText);
+            messageWriter.EndMessage();
+
+            __result = true;
+            return false;
+        }
+    }
+
+
+    // Allow URLs in messages
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.SendFreeChat))]
+    public static class AllowURLS
+    {
+        public static bool Prefix(ChatController __instance)
+        {
+            // Remove deletion of any URLs
+            string text = __instance.freeChatField.Text;
+            ChatController.Logger.Debug("SendFreeChat () :: Sending message: '" + text + "'", null);
+            PlayerControl.LocalPlayer.RpcSendChat(text);
+            return false;
+        }
+    }
+
+
+    // Allow special characters
+    [HarmonyPatch(typeof(TextBoxTMP), nameof(TextBoxTMP.IsCharAllowed))]
+    public static class AllowAllCharacters
+    {
+        public static bool Prefix(TextBoxTMP __instance, ref bool __result)
+        {
+            // Set IsCharAllowed to always return true => every character is allowed
+            __result = true;
+            return false;
+        }
+
+        public static void Postfix(TextBoxTMP __instance)
+        {
+            __instance.allowAllCharacters = true; // not used by game's code, but I still including anyway :)
+            __instance.AllowEmail = true; // self-explanatory
+            __instance.AllowPaste = true;
+            __instance.AllowSymbols = true;
+        }
+
+        // Fix weird backspace bug that occurs when allowing any symbol
+        public static bool Prefix(TextBoxTMP __instance, char i, ref bool __result)
+        {
+            __result = !(i == '\b');
+            return false;
         }
     }
 }
