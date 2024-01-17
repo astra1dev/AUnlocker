c5dc19843a80af932a282f3a7a4b8c55eff8c161
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2024-01-17 19:00:41 +0100

    Create CosmeticsPatches.cs

---
+++ source/CosmeticsPatches.cs
@@ -0,0 +1,42 @@
+using HarmonyLib;
+
+namespace AUnlocker;
+
+
+[HarmonyPatch(typeof(HatManager), nameof(HatManager.Initialize))]
+public static class UnlockCosmetics
+{
+    // loop through all cosmetics and set them free
+    public static void Postfix(HatManager __instance)
+    {
+        foreach (var bundle in __instance.allBundles)
+        { bundle.Free = true; }
+
+        foreach (var featuredBundle in __instance.allFeaturedBundles)
+        { featuredBundle.Free = true; }
+
+        foreach (var featuredCube in __instance.allFeaturedCubes)
+        { featuredCube.Free = true; }
+
+        foreach (var featuredItem in __instance.allFeaturedItems)
+        { featuredItem.Free = true; }
+
+        foreach (var hat in __instance.allHats)
+        { hat.Free = true; }
+
+        foreach (var nameplate in __instance.allNamePlates)
+        { nameplate.Free = true; }
+
+        foreach (var pet in __instance.allPets)
+        { pet.Free = true; }
+
+        foreach (var skin in __instance.allSkins)
+        { skin.Free = true; }
+
+        foreach (var starBundle in __instance.allStarBundles)
+        { starBundle.price = 0; }
+
+        foreach (var visor in __instance.allVisors)
+        { visor.Free = true; }
+    }
+}
