4fc69e9a494f9722d233516d7484f5ffc019ed86
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2024-01-17 19:01:47 +0100

    Create AccountPatches.cs

---
+++ source/AccountPatches.cs
@@ -0,0 +1,82 @@
+using HarmonyLib;
+
+namespace AUnlocker;
+
+
+// Unlock freechat
+[HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFreechatAllowed))]
+public static class UnlockFreechat
+{
+    public static bool Prefix(EOSManager __instance, ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+
+// Unlock friend list
+[HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFriendsListAllowed))]
+public static class UnlockFriendlist
+{
+    public static bool Prefix(EOSManager __instance, ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+
+// Remove minor status
+[HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsMinorOrWaiting))]
+public static class RemoveMinorStatus
+{
+    public static bool Prefix(EOSManager __instance, ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+}
+
+
+// Unlock online gameplay
+[HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsAllowedOnline))]
+public static class OnlineGameplay
+{
+    public static void Prefix(ref bool canOnline)
+    {
+        canOnline = true;
+    }
+}
+
+
+[HarmonyPatch(typeof(AccountManager), nameof(AccountManager.CanPlayOnline))]
+public static class OnlineGameplay2
+{
+    public static void Postfix(ref bool __result)
+    {
+            __result = true;     
+    }
+}
+
+
+// Change login status to "logged in" to allow joining public games
+[HarmonyPatch(typeof(InnerNet.InnerNetClient), nameof(InnerNet.InnerNetClient.JoinGame))]
+public static class CanJoinGame
+{
+    public static void Prefix()
+    {
+        AmongUs.Data.DataManager.Player.Account.LoginStatus = EOSManager.AccountLoginStatus.LoggedIn;
+    }
+}
+
+
+// Unlock custom name
+[HarmonyPatch(typeof(FullAccount), nameof(FullAccount.CanSetCustomName))]
+public static class CustomNameEnabled
+{
+    public static void Prefix(ref bool canSetName)
+    {
+        canSetName = true;
+    }
+}
