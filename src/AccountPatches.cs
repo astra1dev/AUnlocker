fafd230cdc0a89f1bda53022b0bbfb8926190326
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2025-04-18 19:44:59 +0200

    improved removePenalty

--- src/AccountPatches.cs
+++ src/AccountPatches.cs
@@ -116,19 +116,23 @@ public static class SetLoggedIn_InnerNetClient_JoinGame_Prefix
     }
 }
 
-[HarmonyPatch(typeof(PlayerBanData), nameof(PlayerBanData.BanMinutesLeft), MethodType.Getter)]
-public static class RemoveDisconnectPenalty_PlayerBanData_BanMinutesLeft_Postfix
+[HarmonyPatch(typeof(PlayerBanData), nameof(PlayerBanData.BanPoints), MethodType.Setter)]
+public static class RemoveDisconnectPenalty_PlayerBanData_BanMinutesLeft_Prefix
 {
     /// <summary>
     /// Remove the time penalty after disconnecting from too many lobbies.
     /// </summary>
     /// <param name="__instance">The <c>PlayerBanData</c> instance.</param>
-    /// <param name="__result">Original return value of <c>BanMinutesLeft</c>.</param>
-    public static void Postfix(PlayerBanData __instance, ref int __result)
+    /// <param name="value">The value being set to BanPoints.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(PlayerBanData __instance, ref float value)
     {
-        if (!AUnlocker.RemovePenalty.Value) return;
+        if (!AUnlocker.RemovePenalty.Value) return true;
+        if (!(bool) (UnityEngine.Object) AmongUsClient.Instance || AmongUsClient.Instance.NetworkMode != NetworkModes.OnlineGame)
+            return true;
 
-        __instance.BanPoints = 0f; // Remove all BanPoints
-        __result = 0;              // Remove all BanMinutes
+        value = 0f;
+        //__instance.BanPoints = 0f; // Remove all BanPoints
+        return false;
     }
 }
