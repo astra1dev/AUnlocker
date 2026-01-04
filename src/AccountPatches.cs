cdc86c5884739e7faf0d698540e6e28d45222d2b
ducthoe <mariofatrio@gmail.com>
2026-01-04 20:52:05 +0100

    Add KWS Age spoofer
    Pretty shit, vibecoded have to admit it but it works

--- src/AccountPatches.cs
+++ src/AccountPatches.cs
@@ -1,9 +1,192 @@
 using AmongUs.Data.Player;
 using HarmonyLib;
+using Epic.OnlineServices;
+using Epic.OnlineServices.KWS;
 
 namespace AUnlocker;
 
 // Some of the below patches are from https://github.com/scp222thj/MalumMenu/
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.QueryAgeGate))]
+public static class QueryAgeGate_KWSInterface_QueryAgeGate_Prefix
+{
+    /// <summary>
+    /// Spoof age gate queries to return adult status.
+    /// </summary>
+    /// <param name="options">Query options.</param>
+    /// <param name="clientData">Client data.</param>
+    /// <param name="completionDelegate">Completion callback.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref QueryAgeGateOptions options, object clientData, OnQueryAgeGateCallback completionDelegate)
+    {
+        if (!AUnlocker.UnlockMinor.Value) return true;
+
+        try
+        {
+            var spoofedInfo = new QueryAgeGateCallbackInfo
+            {
+                ResultCode = Result.Success,
+                CountryCode = (Utf8String)"US",
+                AgeOfConsent = 18
+            };
+
+            completionDelegate?.Invoke(ref spoofedInfo);
+            return false;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.QueryPermissions))]
+public static class QueryPermissions_KWSInterface_QueryPermissions_Prefix
+{
+    /// <summary>
+    /// Spoof permission queries to return adult status.
+    /// </summary>
+    /// <param name="options">Query options.</param>
+    /// <param name="clientData">Client data.</param>
+    /// <param name="completionDelegate">Completion callback.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref QueryPermissionsOptions options, object clientData, OnQueryPermissionsCallback completionDelegate)
+    {
+        if (!AUnlocker.UnlockMinor.Value) return true;
+
+        try
+        {
+            var spoofedInfo = new QueryPermissionsCallbackInfo
+            {
+                ResultCode = Result.Success,
+                IsMinor = false,
+                DateOfBirth = (Utf8String)"1990-01-01",
+                ParentEmail = (Utf8String)"verified@parental-consent.com"
+            };
+
+            completionDelegate?.Invoke(ref spoofedInfo);
+            return false;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.GetPermissionByKey))]
+public static class GetPermissionByKey_KWSInterface_GetPermissionByKey_Prefix
+{
+    /// <summary>
+    /// Spoof permission checks to always return granted.
+    /// </summary>
+    /// <param name="options">Query options.</param>
+    /// <param name="outPermission">Output permission status.</param>
+    /// <param name="__result">Original return value.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref GetPermissionByKeyOptions options, out KWSPermissionStatus outPermission, ref Result __result)
+    {
+        if (AUnlocker.UnlockMinor.Value)
+        {
+            outPermission = KWSPermissionStatus.Granted;
+            __result = Result.Success;
+            return false;
+        }
+        
+        outPermission = default;
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.CopyPermissionByIndex))]
+public static class CopyPermissionByIndex_KWSInterface_CopyPermissionByIndex_Prefix
+{
+    /// <summary>
+    /// Spoof indexed permission lookups to always return granted.
+    /// </summary>
+    /// <param name="options">Query options.</param>
+    /// <param name="outPermission">Output permission status.</param>
+    /// <param name="__result">Original return value.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref CopyPermissionByIndexOptions options, out PermissionStatus outPermission, ref Result __result)
+    {
+        if (AUnlocker.UnlockMinor.Value)
+        {
+            outPermission = new PermissionStatus
+            {
+                Status = KWSPermissionStatus.Granted
+            };
+            __result = Result.Success;
+            return false;
+        }
+        
+        outPermission = default;
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.RequestPermissions))]
+public static class RequestPermissions_KWSInterface_RequestPermissions_Prefix
+{
+    /// <summary>
+    /// Spoof permission requests to always return success.
+    /// </summary>
+    /// <param name="options">Request options.</param>
+    /// <param name="clientData">Client data.</param>
+    /// <param name="completionDelegate">Completion callback.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref RequestPermissionsOptions options, object clientData, OnRequestPermissionsCallback completionDelegate)
+    {
+        if (!AUnlocker.UnlockMinor.Value) return true;
+
+        try
+        {
+            var spoofedInfo = new RequestPermissionsCallbackInfo
+            {
+                ResultCode = Result.Success
+            };
+
+            completionDelegate?.Invoke(ref spoofedInfo);
+            return false;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(KWSInterface), nameof(KWSInterface.CreateUser))]
+public static class CreateUser_KWSInterface_CreateUser_Prefix
+{
+    /// <summary>
+    /// Spoof user creation to return adult status.
+    /// </summary>
+    /// <param name="options">Creation options.</param>
+    /// <param name="clientData">Client data.</param>
+    /// <param name="completionDelegate">Completion callback.</param>
+    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
+    public static bool Prefix(ref CreateUserOptions options, object clientData, OnCreateUserCallback completionDelegate)
+    {
+        if (!AUnlocker.UnlockMinor.Value) return true;
+
+        try
+        {
+            var spoofedInfo = new CreateUserCallbackInfo
+            {
+                ResultCode = Result.Success,
+                IsMinor = false
+            };
+
+            completionDelegate?.Invoke(ref spoofedInfo);
+            return false;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+}
 
 [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.IsFreechatAllowed))]
 public static class UnlockFreechat_EOSManager_IsFreechatAllowed_Postfix
